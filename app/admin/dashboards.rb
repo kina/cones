52c5e5f815f8ab00ac8e24c0401db4ba0f9bfcfb
Leandro Maríngolo <leandronsp@gmail.com>
2012-07-17 22:34:08 -0300

    customize dashboard

--- app/admin/dashboards.rb
+++ app/admin/dashboards.rb
@@ -1,20 +1,29 @@
 ActiveAdmin::Dashboards.build do
-
   # Define your dashboard sections here. Each block will be
   # rendered on the dashboard in the context of the view. So just
   # return the content which you would like to display.
-  
+
   # == Simple Dashboard Section
   # Here is an example of a simple dashboard section
   #
-  #   section "Recent Posts" do
-  #     ul do
-  #       Post.recent(5).collect do |post|
-  #         li link_to(post.title, admin_post_path(post))
-  #       end
-  #     end
-  #   end
-  
+
+    section "TOP Clientes" do
+      customers = Customer.all.sort_by {|c| c.total_paid_orders}.reverse.take(4)
+      table_for customers do |t|
+        t.column("Nome") { |c| link_to(c.name, admin_customer_path(c)) }
+        t.column("Valor") { |c| c.total_paid_orders}
+      end
+    end
+
+    section "Ultimas Vendas" do
+      table_for Order.order("created_at DESC").limit(4) do |t|
+        t.column("Produto") { |o| link_to(o.product.name, admin_product_path(o.product)) }
+        t.column("Cliente") { |o| link_to(o.customer.name, admin_customer_path(o.customer))}
+        t.column("Valor") { |o| o.total }
+        t.column("Data") { |o| o.created_at.strftime("%d/%m/%Y %H:%M") }
+      end
+    end
+
   # == Render Partial Section
   # The block is rendered within the context of the view, so you can
   # easily render a partial rather than build content in ruby.
@@ -24,7 +33,7 @@ ActiveAdmin::Dashboards.build do
   #       render 'recent_posts' # => this will render /app/views/admin/dashboard/_recent_posts.html.erb
   #     end
   #   end
-  
+
   # == Section Ordering
   # The dashboard sections are ordered by a given priority from top left to
   # bottom right. The default priority is 10. By giving a section numerically lower
@@ -34,7 +43,7 @@ ActiveAdmin::Dashboards.build do
   #   section "Recent User", :priority => 1
   #
   # Will render the "Recent Users" then the "Recent Posts" sections on the dashboard.
-  
+
   # == Conditionally Display
   # Provide a method name or Proc object to conditionally render a section at run time.
   #
