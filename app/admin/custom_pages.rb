508fde879ed5d326284e047b5db606b467c78783
Leandro Maríngolo <leandronsp@gmail.com>
2012-07-19 15:25:09 -0300

    action_item for the new pages

--- app/admin/custom_pages.rb
+++ app/admin/custom_pages.rb
@@ -1,25 +1,24 @@
 ActiveAdmin.register_page 'Sales By Date' do
   menu label: "Vendas por data", parent: "Orders"
+  action_item { link_to "Nova Venda", new_admin_order_path }
+
   content do
-    section "Vendas por data" do
-      orders = Order.sales_by_date
-      table_for orders do
-        column("Data") { |o| o.created_at.strftime("%d/%m/%Y")}
-        column("Total vendido") { |o| o.quantity * o.payment_value }
-      end
+    orders = Order.sales_by_date
+    table_for orders do
+      column("Data") { |o| o.created_at.strftime("%d/%m/%Y")}
+      column("Total vendido") { |o| o.quantity * o.payment_value }
     end
   end
 end
 
 ActiveAdmin.register_page 'Debtors' do
   menu label: "A receber", parent: "Orders"
+  action_item { link_to "Nova Venda", new_admin_order_path }
   content do
-    section "A receber", priority: 3 do
-      customers = Customer.debtors
-      table_for customers do
-        column("Nome") { |c| link_to(c.name, admin_customer_path(c)) }
-        column("Valor") { |c| c.balance}
-      end
+    customers = Customer.debtors
+    table_for customers do
+      column("Nome") { |c| link_to(c.name, admin_customer_path(c)) }
+      column("Valor") { |c| c.balance}
     end
   end
 end
